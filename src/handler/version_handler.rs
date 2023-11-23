300cea1c17860a1ef48da0a5069f7b0ed09f3a8a
Thomas Pham <phamtri2895@gmail.com>
2023-11-23 07:16:53 +0700

    Add files via upload

--- src/handler/version_handler.rs
+++ src/handler/version_handler.rs
@@ -12,7 +12,7 @@ use crate::response::api_response::*;
 
 pub async fn get_version() -> (StatusCode, Json<GenericResponse<'static>>)  {
     let json_response = GenericResponse {
-        status: "",
+        status: STATUS_MAPPER.get(&STATUS_NO_ERROR).unwrap_or(&STATUS_NO_ERROR_STR),
         status_code: STATUS_NO_ERROR,
         message: "1.0.0",
         data: HashMap::new(),
--- src/response/api_response.rs
+++ src/response/api_response.rs
@@ -5,7 +5,7 @@ use axum::Json;
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use serde_json::{Value, Map, Number};
-
+use once_cell::sync::Lazy;
 
 pub const STATUS_NO_ERROR: i8 = 0;
 pub const STATUS_BAD_REQUEST: i8 = 1;
@@ -13,14 +13,20 @@ pub const STATUS_REQUEST_TIMEOUT_ERROR: i8 = 2;
 pub const STATUS_INTERNAL_SERVER_ERROR: i8 = 3;
 
 
-pub static STATUS_MAPPER: HashMap<i8, &str> = HashMap::from(
+pub const STATUS_NO_ERROR_STR: &str = "OK";
+pub const STATUS_BAD_REQUEST_STR: &str = "Bad Request";
+pub const STATUS_REQUEST_TIMEOUT_ERROR_STR: &str = "Request Timeout";
+pub const STATUS_INTERNAL_SERVER_ERROR_STR: &str = "Internal Server Error";
+
+
+pub static STATUS_MAPPER: Lazy<HashMap<i8, &str>> = Lazy::new(|| HashMap::from(
     [
-        (STATUS_NO_ERROR, "OK"),
-        (STATUS_BAD_REQUEST, "Bad Request"),
-        (STATUS_REQUEST_TIMEOUT_ERROR, "Request Timeout"),
-        (STATUS_INTERNAL_SERVER_ERROR, "Internal Server Error"),
+        (STATUS_NO_ERROR, STATUS_NO_ERROR_STR),
+        (STATUS_BAD_REQUEST, STATUS_BAD_REQUEST_STR),
+        (STATUS_REQUEST_TIMEOUT_ERROR, STATUS_REQUEST_TIMEOUT_ERROR_STR),
+        (STATUS_INTERNAL_SERVER_ERROR, STATUS_INTERNAL_SERVER_ERROR_STR),
     ],
-);
+));
 
 /// Define the common response for all api call
 #[derive(Debug, Serialize)]
